3f4cbe07dcf2636955b66348e0f2b5eaeb977375
theverything <j3ffhorn@gmail.com>
2014-07-05 18:12:25 -0700

    add naught

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var server = require('./server/server');
 
 server.start(function () {
+  if (process.send) process.send('online');
   console.log("Hapi server started @", server.info.uri);
+});
+
+process.on('message', function(message) {
+  if (message === 'shutdown') {
+    process.exit(0);
+  }
 });
---
+++ build/deploy.sh
@@ -0,0 +1,3 @@
+cd /home/kp-web && \
+  git pull && \
+  naught deploy --worker-count 2
